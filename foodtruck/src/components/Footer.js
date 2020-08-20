95107b3abb6a6a2fc373561d5fb9c5ba0b2b4a2c
harvey magana <heyharv2012@gmail.com>
2020-08-19 21:21:02 -0700

    Added react-router-dom to the project and to index.js

--- foodtruck/src/components/Footer.js
+++ foodtruck/src/components/Footer.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
 import '../App.css';
 
 const useStyles = makeStyles((theme) => ({
