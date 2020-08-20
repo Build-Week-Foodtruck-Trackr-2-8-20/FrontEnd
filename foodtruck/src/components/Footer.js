004c5e8ba09007b6951dad3cf44b64bf656dbe9c
harvey magana <heyharv2012@gmail.com>
2020-08-19 21:07:03 -0700

    Added the header and footer components to the project.

--- foodtruck/src/components/Footer.js
+++ foodtruck/src/components/Footer.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
 import '../App.css';
 
-function Footer() {
-  return (
-    <div className="App">
-        Footer
-    </div>
-  );
-}
+const useStyles = makeStyles((theme) => ({
+    text: {
+      padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+      paddingBottom: 50,
+    },
+    list: {
+      marginBottom: theme.spacing(2),
+    },
+    subheader: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    fabButton: {
+      position: 'absolute',
+      zIndex: 1,
+      top: -30,
+      left: 0,
+      right: 0,
+      margin: '0 auto',
+    },
+  }));
 
-export default Footer;
+  export default function Footer() {
+    const classes = useStyles();
+  
+    return (
+      <React.Fragment>
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Toolbar>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
+  }
--- foodtruck/src/components/Header.js
+++ foodtruck/src/components/Header.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import '../App.css';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    toolbar: {
+      minHeight: 128,
+      alignItems: 'flex-start',
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      alignSelf: 'flex-end',
+    },
+  }));
 function Header() {
   return (
     <div className="App">
@@ -9,4 +32,16 @@ function Header() {
   );
 }
 
-export default Header;
+export default function ProminentAppBar() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" position="fixed">
+          <Toolbar className={classes.toolbar}>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+  
