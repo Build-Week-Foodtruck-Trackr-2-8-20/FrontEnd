07543a34641875335ab97b64f8eb72684b80bd12
harvey magana <heyharv2012@gmail.com>
2020-08-19 22:18:54 -0700

    Fixed the header of the project. Tried some things that did not work. Trying to keep what is working with what is currently set.

--- foodtruck/src/App.js
+++ foodtruck/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Korean from './components/Korean';
 import BBQ from './components/BBQ';
 import Tacos from './components/Tacos';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Tacos />
-      <BBQ />
-      <Korean />
+      <Switch>
+        <Route path="/korean" render={() => <Korean />} />
+        <Route path="/bbq" render={() => <BBQ />} />
+        <Route path="/tacos" render={() => <Tacos />} />
+        <Route path="/" component={Home} />
+      </Switch>
       <Footer />
     </div>
   );
