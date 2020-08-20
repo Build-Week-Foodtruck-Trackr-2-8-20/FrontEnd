48c0cc4d2213b6612f4cdfc5f0ad65424819b870
harvey magana <heyharv2012@gmail.com>
2020-08-19 19:55:50 -0700

    Added the components to the App component.

--- foodtruck/src/App.js
+++ foodtruck/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Korean from './components/Korean';
+import BBQ from './components/BBQ';
+import Tacos from './components/Tacos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <Tacos />
+      <BBQ />
+      <Korean />
+      <Footer />
     </div>
   );
 }
