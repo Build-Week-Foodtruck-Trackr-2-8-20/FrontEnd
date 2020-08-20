2ea997ed9cb29171ec18a462f65a1938c3519551
harvey magana <heyharv2012@gmail.com>
2020-08-19 19:53:01 -0700

    Added code to the components.

---
+++ foodtruck/src/components/Korean.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Korean() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Korean;
---
+++ foodtruck/src/components/Tacos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Tacos() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Tacos;
