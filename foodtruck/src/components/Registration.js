491de2e3058979b40faed3024a5e6a293447474b
harvey magana <heyharv2012@gmail.com>
2020-08-21 22:11:19 -0700

    Submit button added to the form.

--- foodtruck/src/components/Registration.js
+++ foodtruck/src/components/Registration.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
-import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import '../App.css';
 
@@ -21,6 +24,11 @@ const useStyles = makeStyles({
 
 function Registration() {
     const classes = useStyles();
+    const [value, setValue] = React.useState('diner');
+
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
 
   return (
     <Container>
@@ -30,10 +38,13 @@ function Registration() {
             <Input id="userNameInput" aria-describedby="my-helper-text" />
             <InputLabel  className={classes.input} htmlFor="emailInput">Email address</InputLabel>
             <Input id="emailInput" aria-describedby="my-helper-text" />
-            <InputLabel className={classes.input} htmlFor="userInput">End User Type</InputLabel>
-            <Input id="userInput" aria-describedby="my-helper-text" />
+            <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                <FormControlLabel value="diner" control={<Radio />} label="Diner" />
+                <FormControlLabel value="operator" control={<Radio />} label="Operator" />
+            </RadioGroup>
             <InputLabel className={classes.input} htmlFor="passwordInput">Password</InputLabel>
             <Input id="passwordInput" aria-describedby="my-helper-text" />
+            <Button>Submit</Button>
         </FormControl>
     </Container>
   );
