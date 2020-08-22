de7c1dd333c1ac9a7bcb83876f3341c58728d2ab
harvey magana <heyharv2012@gmail.com>
2020-08-21 18:30:19 -0700

    Updated the heaer with a hamburger navigation.

--- foodtruck/src/components/Header.js
+++ foodtruck/src/components/Header.js
@@ -1,61 +1,160 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    background: '#cc0000',
+    display: 'flex',
+    background: '#cc0000'
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
   },
   title: {
     flexGrow: 1,
   },
-  button: {
-    margin: '10px',
-    backgroundColor: '#fedc56',
-    color: '#ddd'
+  hide: {
+    display: 'none',
   },
-  link: {
-      textDecoration:'none'
-  }
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
 }));
 
-export default function ButtonAppBar() {
+
+
+export default function Header() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.root}>
-        <Toolbar color="danger">
-          <Typography variant="h6" className={classes.title}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+        className={classes.root}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap className={classes.title}>
             Food Truck App
           </Typography>
-          <Button className={classes.button}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            className={clsx(open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+        <ListItem>
             <Link to="/" className={classes.link}>
                 Home
             </Link>
-          </Button>
-          <Button className={classes.button}>
-            <Link to="/tacos" className={classes.link}>
+        </ListItem>
+        <ListItem>
+        <Link to="/tacos" className={classes.link}>
                 Tacos
             </Link>
-          </Button>
-          <Button className={classes.button}>
-            <Link to="/bbq" className={classes.link}>
+        </ListItem>
+        <ListItem>
+        <Link to="/bbq" className={classes.link}>
                 BBQ
             </Link>
-          </Button>
-          <Button className={classes.button}>
-            <Link to="/korean" className={classes.link}>
+        </ListItem>
+        <ListItem>
+        <Link to="/korean" className={classes.link}>
                 Korean
             </Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+        </ListItem>
+        </List>
+      </Drawer>
     </div>
   );
 }
