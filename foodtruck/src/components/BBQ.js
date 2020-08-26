fa87454a7116608a03a95d4bffc483e4a1b540cd
harvey magana <heyharv2012@gmail.com>
2020-08-25 22:11:23 -0700

    Added a header image to the BBQ component.

--- foodtruck/src/components/BBQ.js
+++ foodtruck/src/components/BBQ.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
 import '../App.css';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#8B4513",
+    margin: "10px auto",
+    padding: "10px",
+    width: "100%",
+    height: '100vh'
+  }, 
+  header: {
+    margin: '50px auto 5px',
+    padding: '0px'
+  }, 
+  image: {
+    width: '100%', 
+    borderRadius: '0px', 
+    marginTop: '0px'
+  }
+});
+
 function BBQ() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Container maxWidth="lg">
           BBQ
-        <Typography component="div" style={{ backgroundColor: '#8B4513', height: '100vh' }} />
+          <Container className={classes.header}>
+          <img className={classes.image} src="http://placehold.it/1000x300"
+              srcset="
+                http://placehold.it/1000x300 300w"
+              sizes="
+                100vw"
+              alt="Placeholder"
+          />  
+          </Container>
+        <Typography component="div" className={classes.root} />
       </Container>
     </React.Fragment>
   );
