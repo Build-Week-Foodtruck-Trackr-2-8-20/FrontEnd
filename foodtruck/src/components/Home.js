c182b5a979018ac95bf19a084e521ec2aa0a732e
harvey magana <heyharv2012@gmail.com>
2020-08-25 22:17:41 -0700

    Added header image and spacing style to the Home, Taco, Korean and BBQ components.

--- foodtruck/src/components/Home.js
+++ foodtruck/src/components/Home.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
 import '../App.css';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#eee",
+    margin: "10px auto",
+    padding: "10px",
+    width: "100%",
+    height: '100vh'
+  }, 
+  header: {
+    margin: '50px auto 5px',
+    padding: '0px'
+  }, 
+  image: {
+    width: '100%', 
+    borderRadius: '0px', 
+    marginTop: '0px'
+  }
+});
+
 function Home() {
+  const classes = useStyles();
+  
   return (
     <React.Fragment>
-      <Container maxWidth="lg" component="div" style={{ backgroundColor: '#eee', height: '100vh' }}>
-        <Typography>Home</Typography>
+      <Container maxWidth="lg">
+          BBQ
+          <Container className={classes.header}>
+          <img className={classes.image} src="http://placehold.it/1000x300"
+              srcset="
+                http://placehold.it/1000x300 300w"
+              sizes="
+                100vw"
+              alt="Placeholder"
+          />  
+          </Container>
+        <Typography component="div" className={classes.root} />
       </Container>
     </React.Fragment>
   );
--- foodtruck/src/components/Tacos.js
+++ foodtruck/src/components/Tacos.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from "@material-ui/core/styles";
 import '../App.css';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "green",
+    margin: "10px auto",
+    padding: "10px",
+    width: "100%",
+    height: '100vh'
+  }, 
+  header: {
+    margin: '50px auto 5px',
+    padding: '0px'
+  }, 
+  image: {
+    width: '100%', 
+    borderRadius: '0px', 
+    marginTop: '0px'
+  }
+});
+
 function Tacos() {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Container maxWidth="lg">
-          Tacos
-        <Typography component="div" style={{ backgroundColor: '#006200', height: '100vh' }} />
+          BBQ
+          <Container className={classes.header}>
+          <img className={classes.image} src="http://placehold.it/1000x300"
+              srcset="
+                http://placehold.it/1000x300 300w"
+              sizes="
+                100vw"
+              alt="Placeholder"
+          />  
+          </Container>
+        <Typography component="div" className={classes.root} />
       </Container>
     </React.Fragment>
   );
