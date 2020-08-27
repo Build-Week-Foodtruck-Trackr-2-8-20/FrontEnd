04ae819e73f002a2a2794a5a1a899dd52521b478
sam hall <sam_h_hall@icloud.com>
2020-08-27 15:54:15 -0500

    added redirect to Registration, operator actions

--- foodtruck/src/actions/userActions.js
+++ foodtruck/src/actions/userActions.js
@@ -13,8 +13,12 @@ export const LOGIN_OPERATOR_SUCCESS = "LOGIN_OPERATOR_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const ADD_GPS_LOCATION = "ADD_GPS_LOCATION";
 export const ADD_MANUAL_LOCATION = "ADD_MANUAL_LOCATION";
+export const ADD_TRUCK = "ADD_TRUCK";
+export const ADD_MENU_ITEM = "ADD_MENU_ITEM";
+export const ADD_MENU_ITEM_PHOTO = "ADD_MENU_ITEM_PHOTO";
 export const ADD_FAVORITE = "ADD_FAVORITE";
 export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
+
 
 export const registerUser = (registerObj) => (dispatch) => {
   console.log("userActions.js: registerUser: registerObj: ", registerObj);
@@ -68,9 +72,46 @@ export const loginUser = (loginObj) => (dispatch) => {
     });
 };
 
-// export const addTruck = (truckObj) => (dispatch) => {
+// OPERATOR EXCLUSIVE ACTIONS
 
-// }
+// truckObj = {imageURL: "", cuisineType: "", location: "", locationGPS: "", departureTime: "", customerRatingAvg: 0, username: " //username of operator "}
+export const addTruck = (truckObj) => (dispatch) => {
+  axiosWithAuth()
+    .post("https://food-truck-lambda.herokuapp.com/api/trucks", truckObj)
+    .then((res) => {
+      dispatch({
+        type: ADD_TRUCK,
+        payload: res.data
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+export const addMenuItem = (menuObj) => (dispatch) => {
+  axiosWithAuth()
+    .post("https://food-truck-lambda.herokuapp.com/api/trucks/menuItems", menuObj)
+    .then((res) => {
+      dispatch({
+        type: ADD_MENU_ITEM,
+        payload: res.data
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+export const addMenuItemPhoto = (photoObj) => (dispatch) => {
+  axiosWithAuth()
+    .post(`https://food-truck-lambda.herokuapp.com/api/menuitems/${photoObj.id}/photos`, photoObj)
+    .then((res) => {
+      dispatch({
+        type: ADD_MENU_ITEM_PHOTO,
+        payload: res.data
+      })
+    })
+    .catch((err) => console.log(err))
+}
+
+// DINER EXCLUSIVE ACTIONS
 
 // dinerTruckIDsObj = {userid: 3, truckid: 2}
 export const addFavorite = (dinerTruckIDsObj) => (dispatch) => {
@@ -82,9 +123,7 @@ export const addFavorite = (dinerTruckIDsObj) => (dispatch) => {
         payload: res.data
       })
     })
-    .catch(err => {
-      console.log(err)
-    })
+    .catch(err => console.log(err))
 };
 
 // dinerTruckIDsObj = {userid: 3, truckid: 2}
@@ -97,7 +136,5 @@ export const removeFavorite = (dinerTruckIDsObj) => (dispatch) => {
         payload: res.data
       })
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
