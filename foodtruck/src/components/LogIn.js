68e7a1747b016968386306a0f41f770c0bb253bc
Tgleason1980 <terryscoding@gmail.com>
2020-08-26 21:53:28 -0500

    fixing errors

--- foodtruck/src/components/LogIn.js
+++ foodtruck/src/components/LogIn.js
@@ -1,107 +1,97 @@
 import React, { useState, Fragment, useEffect } from "react";
-import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core/styles";
 import { Formik, Field } from "formik";
 import * as yup from "yup";
-import { connect } from "react-redux";
-import { loginUser } from "../actions/userActions";
 
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: "#ddd",
-    margin: "100px auto",
-    padding: "10px",
-    width: "100%",
-  },
-});
 
 const intialState = {
-  username: "",
+
+  email: "",
   password: ""
 };
-
 const userSchema = yup.object().shape({
-    username: yup.string().required(),
-  password: yup.string().required().max(13).min(8),
+
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup
+    .string()
+    .required()
+    .max(13)
+    .min(8)
 });
 
-function LogIn(props) {
+
+function Login(props) {
   const [user, setUser] = useState(intialState);
-  const classes = useStyles();
-  console.log(user);
   return (
- 
-    <Container className={classes.root}>
       <div className="loginForm">
         <Fragment>
-          <Formik
+        
+        <Formik
             initialValues={user}
             onSubmit={(values, actions) => {
-              actions.setSubmitting(true);
-              // props.loginUser(values);
-              setUser(values);
-              setTimeout(() => {
+            actions.setSubmitting(true);
+            setUser(values);
+            setTimeout(() => {
                 actions.setSubmitting(false);
-              }, 500);
+            }, 500);
             }}
-            validationSchema={userSchema}>
-            {(props) =>
-              !props.isSubmitting ? (
+            validationSchema={userSchema}
+        >
+            {props =>
+            !props.isSubmitting ? (
                 <form onSubmit={props.handleSubmit} className="">
-                  <Field
+                <Field
                     type="email"
                     placeholder="Enter email"
                     onChange={props.handleChange}
                     name="email"
                     value={props.values.email}
                     className="textfield"
-                  />
+                />
 
-                  {props.errors.email && props.touched.email ? (
+                {props.errors.email && props.touched.email ? (
                     <span className="textfield">{props.errors.email}</span>
-                  ) : (
+                ) : (
                     ""
-                  )}
+                )}
 
-                  <Field
+                <Field
                     type="password"
                     onChange={props.handleChange}
                     name="password"
                     value={props.values.password}
                     placeholder="Password"
                     className="textfield"
-                  />
+                />
 
-                  {props.errors.password && props.touched.password ? (
+                {props.errors.password && props.touched.password ? (
                     <span className="fieldtext">{props.errors.password}</span>
-                  ) : (
+                ) : (
                     ""
-                  )}
-
-                  <button
+                )}
+            
+                <button
                     type="submit"
                     disabled={!props.dirty && props.isSubmitting}
-                    className="submitButton">
+                    className="submitButton"
+                >
                     Login
-                  </button>
+                </button>
+                
                 </form>
-              ) : (
+            ) : (
                 <span className="user">{JSON.stringify(user, null, 2)}</span>
-              )
+            
+                
+            )
             }
-          </Formik>
+        </Formik>
         </Fragment>
-      </div>
-    </Container>
+    </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    state,
-  };
-};
+export default Login;
 
-
-export default connect(mapStateToProps, { loginUser })(LogIn);
